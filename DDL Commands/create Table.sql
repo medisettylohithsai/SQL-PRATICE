bbe9aefda8e53109ca71475ec8b44d2a0f395f4a
LohithSaiMedisetty <157821306+medisettylohithsai@users.noreply.github.com>
2026-07-25 09:56:12 +0530

    Add files via upload

---
+++ DDL Commands/create Table.sql
@@ -0,0 +1,21 @@
+/*Create Table
+
+Create an Employees table with the following columns:
+
+EmpID
+EmpName
+Age
+Department
+Salary
+
+Choose appropriate data types.*/
+
+
+create table emp(
+	empid int primary key,
+    empname varchar(25),
+    age int,
+    department varchar(30),
+    salary int
+    );
+
